e09e6003d595b2b4c8a98f6b194514051b1ac743
Ferdinand Köppen <fkoeppen@edu.aau.at>
2019-09-27 03:16:35 +0200

    Added simple scen transition

--- Graeta/Assets/Scripts/Portal/Portal.cs
+++ Graeta/Assets/Scripts/Portal/Portal.cs
@@ -1,14 +1,17 @@
-﻿using UnityEngine;
+﻿using SceneTransition;
+using UnityEngine;
 
 namespace Portal
 {
     public class Portal : MonoBehaviour
     {
+        public SceneNames loadNext;
+        
         private void OnTriggerEnter2D(Collider2D other)
         {
             if (other.tag.Equals("Player"))
             {
-                Debug.Log("Loading Scene");
+                SceneManager.LoadNextScene();
             }
         }
     }
