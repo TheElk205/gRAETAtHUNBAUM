1addaf127f3f6d9595283727698692743c24add0
Ferdinand Köppen <fkoeppen@edu.aau.at>
2019-09-27 14:34:52 +0200

    Can collect reosurces now

---
+++ Graeta/Assets/Scripts/Collectibles/CollectibleResource.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Collectibles
+{
+    public class CollectibleResource : MonoBehaviour
+    {
+        public float resources = 1.0f;
+
+        public void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.gameObject.CompareTag("Player"))
+            {
+                CollectiblesManager.instance.Collected(this);
+            }
+        }
+    }
+}
---
+++ Graeta/Assets/Scripts/Collectibles/CollectiblesManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+namespace Collectibles
+{
+    public class CollectiblesManager : MonoBehaviour
+    {
+        public static CollectiblesManager instance { private set; get; }
+
+        public float collectedResources;
+        
+        public void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+                DontDestroyOnLoad(gameObject);
+            }
+            
+            if (instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+        public void Collected(CollectibleResource resource)
+        {
+            collectedResources += resource.resources;
+            
+            Destroy(resource.gameObject);
+        }
+    }
+}
