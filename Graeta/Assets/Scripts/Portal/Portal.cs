cbe35a18fd21de27ae33b5529dfe9f510f3be85d
Ferdinand Köppen <fkoeppen@edu.aau.at>
2019-09-27 02:03:51 +0200

    Added portal and debug message if hit

---
+++ Graeta/Assets/Scripts/Portal/Portal.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace Portal
+{
+    public class Portal : MonoBehaviour
+    {
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.tag.Equals("Player"))
+            {
+                Debug.Log("Loading Scene");
+            }
+        }
+    }
+}
