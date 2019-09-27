4d1deb8c1fd438b2614a5d3682d16dfe371c726e
Prepe <37080825+Prepe@users.noreply.github.com>
2019-09-27 19:54:51 +0200

    Implemented Controller inputs

---
+++ Graeta/Assets/Scripts/Player/InputManager.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    public KeyCode fireKey;
+    public float wiggleLimit;
+    float wiggleTimer;
+    Vector2 lastInput = new Vector2(0, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public Vector2 MoveDirection()
+    {
+        Vector2 direction;
+
+        direction.x = Input.GetAxis("Horizontal");
+        direction.y = Input.GetAxis("Vertical");
+
+        return direction;
+    }
+
+    public bool StandsStill()
+    {
+        if (MoveDirection().Equals(new Vector2(0, 0)))
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public bool Shoot()
+    {
+        if (Input.GetKeyUp(fireKey)|| Input.GetAxis("Right Trigger") > 0)
+        {
+            return true;
+        }
+
+        return false;
+    }
+
+    public Vector2 ShotDirection()
+    {
+        Vector2 direction = new Vector2(0, 0);
+
+        direction = Input.mousePosition;
+
+        if (Input.GetAxis("Aim X") != 0 || Input.GetAxis("Aim Y") != 0)
+        {
+            direction.x = Input.GetAxis("Aim X");
+            direction.y = Input.GetAxis("Aim Y");
+        }
+
+        return direction;
+    }
+
+    public bool Wiggels()
+    {
+        Vector2 thisInput = MoveDirection();
+
+        if (lastInput == new Vector2(0, 0) && thisInput != new Vector2(0, 0))
+        {
+            lastInput = thisInput;
+            wiggleTimer = 0;
+            return true;
+        }
+
+        if (wiggleTimer > wiggleLimit)
+        {
+            lastInput = new Vector2(0, 0);
+            return false;
+        }
+
+        if (InputIsDiferent(lastInput, thisInput) && thisInput != new Vector2(0, 0))
+        {
+            lastInput = thisInput;
+            wiggleTimer = 0;
+            return true;
+        }
+
+        wiggleTimer += Time.deltaTime;
+        return true;
+    }
+
+    bool InputIsDiferent(Vector2 in1, Vector2 in2)
+    {
+        if (in1.x > 0 && in2.x <= 0)
+            return true;
+
+        if (in1.x < 0 && in2.x >= 0)
+            return true;
+
+        if (in1.x == 0 && in2.x != 0)
+            return true;
+
+        if (in1.y == 0 && in2.y != 0)
+            return true;
+
+        if (in1.y > 0 && in2.y <= 0)
+            return true;
+
+        if (in1.y < 0 && in2.y >= 0)
+            return true;
+
+        return false;
+    }
+}
