7ed116ea8efc4f8bec657736c99867d52ea1cbd7
Ferdinand Köppen <fkoeppen@edu.aau.at>
2019-09-28 12:38:12 +0200

    Started walking

---
+++ Graeta/Assets/Scripts/Navigation/Waypoint.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Navigation
+{
+    public class Waypoint : MonoBehaviour
+    {
+        public List<WaypointConnection> connections;
+
+    }
+
+    [Serializable]
+    public class WaypointConnection
+    {
+        public float weight = 1.0f;
+        public Waypoint pointIngTo;
+    }
+}
