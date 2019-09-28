f17990f7411879896a8b8d8eaf10d012ade50c87
Ferdinand Köppen <fkoeppen@edu.aau.at>
2019-09-28 16:38:33 +0200

    Adddded Sprite Exchange

---
+++ Graeta/Assets/Scripts/GraphicsUtils/SpriteExchangeOnStart.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+namespace GraphicsUtils
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class SpriteExchangeOnStart : MonoBehaviour
+    {
+        public Sprite[] sprites;
+
+        public void Start()
+        {
+            if (sprites == null || sprites.Length == 0)
+            {
+                return;
+            }
+
+            int index = Random.Range(0, sprites.Length);
+            GetComponent<SpriteRenderer>().sprite = sprites[index];
+        }
+    }
+}
